2fa10d17c93f6e835ba49169cacfc13ebd6ef070
Abdulraheem <raimitaiwo45@gmail.com>
2022-10-06 11:34:03 +0100

    new commit

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 let todoItems = [];
-
+localStorage.setItem("todoItemsRef", JSON.stringify(todoItems));
 function renderTodo(todo) {
-    const todoUL = document.querySelector('.ul');
-    const item = document.querySelector(`[data-key='${todo.id}']`);
+  const todoUL = document.querySelector(".ul");
+  const item = document.querySelector(`[data-key='${todo.id}']`);
 
-    if(todo.deleted) {
-        item.remove();
-        return
-    }
+  if (todo.deleted) {
+    item.remove();
+    if (todoItems.length === 0) todoUL.innerHTML = '';
+    return;
+  }
 
-    const isChecked = todo.checked ? 'done': '';
-    
-    const todolist = document.createElement("li");
-    todolist.setAttribute('class', `todo-item ${isChecked}`);
-    todolist.setAttribute('data-key', todo.id)
-    todolist.innerHTML = `
+  const isChecked = todo.checked ? "done" : "";
+
+  const todolist = document.createElement("li");
+  todolist.setAttribute("class", `todo-item ${isChecked}`);
+  todolist.setAttribute("data-key", todo.id);
+  todolist.innerHTML = `
         <input type="checkbox" class="inputCheckbox" id="${todo.id}"/>
         <label for="${todo.id}" class="tick js-tick"></label>
         <span>${todo.todoText}</span>
@@ -22,67 +23,76 @@ function renderTodo(todo) {
         <svg><use href="#delete-icon"</use></svg>
         </button>
     `;
-    // If the item already exists in the DOM
-    if (item){
-        todoUL.replaceChild(todolist, item)
-    } else{
-        todoUL.append(todolist);
-    }
+  // If the item already exists in the DOM
+  if (item) {
+    todoUL.replaceChild(todolist, item);
+  } else {
+    todoUL.append(todolist);
+  }
 }
 
 function addTodo(text) {
-    const todo = {
-        todoText,
-        checked: false,
-        id: Date.now(),
-    };
-    todoItems.push(todo);
-    renderTodo(todo);
-    // console.log(todoItems);
-};
+  const todo = {
+    todoText,
+    checked: false,
+    id: Date.now(),
+  };
+  todoItems.push(todo);
+  renderTodo(todo);
+  // console.log(todoItems);
+}
 
-const addBtn = document.querySelector('.addBtn');
-addBtn.addEventListener('click', (e)=>{
-    e.preventDefault();
-    const todoInput = document.querySelector('.todoInput');
+const addBtn = document.querySelector(".addBtn");
+addBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  const todoInput = document.querySelector(".todoInput");
 
-    todoText = todoInput.value.trim();
-    if (todoText !== ''){
-        addTodo(todoText);
-        todoInput.value = '';
-        todoInput.focus();
-    }
+  todoText = todoInput.value.trim();
+  if (todoText !== "") {
+    addTodo(todoText);
+    todoInput.value = "";
+    todoInput.focus();
+  }
 });
 
 function toggleDone(key) {
-    const index = todoItems.findIndex(item => item.id === Number(key));
-    todoItems[index].checked = !todoItems[index].checked;
-    renderTodo(todoItems[index]);
-} 
-
-function deletTodo(key) {
-    const index = todoItems.findIndex(item => item.id === Number(key));
-    const todo = {
-        deleted: true,
-        ...todoItems[index]
-    };
-    todoItems = todoItems.filter(item => item.id !== Number(key));
-    renderTodo(todo)
+  const index = todoItems.findIndex((item) => item.id === Number(key));
+  todoItems[index].checked = !todoItems[index].checked;
+  renderTodo(todoItems[index]);
 }
 
-const todoUL = document.querySelector('.ul');
-todoUL.addEventListener('click', (e)=>{
-    if (e.target.classList.contains('js-tick')){
-        const itemKey = e.target.parentElement.dataset.key;
-        toggleDone(itemKey);
-    }
+function deletTodo(key) {
+  const index = todoItems.findIndex((item) => item.id === Number(key));
+  const todo = {
+    deleted: true,
+    ...todoItems[index],
+  };
+  todoItems = todoItems.filter((item) => item.id !== Number(key));
+  renderTodo(todo);
+}
 
-    if (e.target.classList.contains('js-delete-todo')){
-        const itemKey = e.target.parentElement.dataset.key;
-        deletTodo(itemKey);
-    }
-})
+const todoUL = document.querySelector(".ul");
+todoUL.addEventListener("click", (e) => {
+  if (e.target.classList.contains("js-tick")) {
+    const itemKey = e.target.parentElement.dataset.key;
+    toggleDone(itemKey);
+  }
 
+  if (e.target.classList.contains("js-delete-todo")) {
+    const itemKey = e.target.parentElement.dataset.key;
+    deletTodo(itemKey);
+  }
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const ref = localStorage.getItem("todoItems");
+  if (ref) {
+    todoItems = JSON.parse(ref);
+    todoItems.forEach((t) => {
+      renderTodo(t);
+    });
+  }
+});
 // const check = document.querySelector(`#${todo.id}`);
 // check.addEventListener('click', (e)=>{
 
